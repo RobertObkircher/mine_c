cba6002ab1efd733a01c645cc2209cf189a80282
Robert Obkircher <robert.obkircher@gmail.com>
2019-01-04 19:20:30 +0100

    fix include guards

--- src/filereload.h
+++ src/filereload.h
@@ -1,8 +1,6 @@
 #ifndef MINE_C_FILERELOAD_H
 #define MINE_C_FILERELOAD_H
 
-#endif //MINE_C_FILERELOAD_H
-
 #define FILERELOAD_MAX_WATCHED_FILES 100
 
 void init_filereload();
@@ -13,3 +11,5 @@ void listen_for_file_changes(const char *directory_name, const char *filename,
                              void (*callback)(char *path));
 
 int update_filereload();
+
+#endif //MINE_C_FILERELOAD_H
